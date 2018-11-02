ea940a17e345c1f85beca27c2f5309c3d62ac28d
Erick_Perez <erickperezcol@gmail.com>
2018-11-02 04:34:05 -0400

    Merge

--- src/app/layout/visita/visita.component.ts
+++ src/app/layout/visita/visita.component.ts
@@ -16,10 +16,49 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 })
 export class VisitaComponent implements OnInit {
 
+    faEye = faEye;
+    faEdit = faEdit;
+    faTrash = faTrash;
 
-  constructor() {
+    showNew: Boolean = false;
+    closeResult: string;
+    submitType: string = 'Guardar';
+    selectedRow: number;
+  constructor(private modalService: NgbModal) {
    
    } 
-    ngOnInit() {}
+    ngOnInit() {
+        
+    }
 
+    onEdit(index: number) {
+        // Change submitType to Update.
+    this.submitType = 'Actualizar';
+    // Display Usuario entry section.
+    this.showNew = true;
+    }
+
+      open(content) {
+        this.modalService.open(content).result.then((result) => {
+
+            this.closeResult = `Closed with: ${result}`;
+             if (this.submitType === 'Guardar') {
+          // Push Usuario model object into Usuario list.
+         
+          console.log(this.submitType);
+        } else {
+          this.submitType = 'Actualizar';
+          // Update the existing properties values based on model.
+         
+         
+    
+        
+        }
+        // Hide Usuario entry section.
+        this.showNew = false;
+            }
+            , (reason) => {
+                
+            });
+    }
 }
