89a55f96cae9ce8981c99a01b4909c4780d047ba
katerinsierra.09 <kasale.0911@gmail.com>
2018-11-08 05:30:03 -0430

    update-module.ts

--- src/app/layout/cliente/perfil/perfil.module.ts
+++ src/app/layout/cliente/perfil/perfil.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+//import { FormsModule } from '@angular/forms';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { FlatpickrModule } from "angularx-flatpickr"
