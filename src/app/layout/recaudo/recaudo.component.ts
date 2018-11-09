f98b8b3614a783d8d9ee07ba7490655f4ccaf344
Pedro Gabriel <gaboleal123@gmail.com>
2018-11-08 22:10:07 -0430

    Update recaudos

--- src/app/layout/recaudo/recaudo.component.ts
+++ src/app/layout/recaudo/recaudo.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { NgbModal, ModalDismissReasons,NgbDatepickerConfig, NgbDateParserFormatter  } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-recaudo',
   templateUrl: './recaudo.component.html',
+  animations: [routerTransition()],
   styleUrls: ['./recaudo.component.scss']
 })
 export class RecaudoComponent implements OnInit {
+closeResult: string; 
+  constructor(private modalService: NgbModal) { }
 
-  constructor() { }
+   open(content) {
+     
+        this.modalService.open(content).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+         
+        }
+        , (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return  `with: ${reason}`;
+        }
+    }
 
   ngOnInit() {
+   
   }
-
+   faEdit = faEdit;
 }
