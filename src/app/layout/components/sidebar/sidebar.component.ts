912994f6937f011f0a25e40351c2e68df7d98e8f
ysidrofernandez <ysidrofernandez3012@gmail.com>
2018-11-23 08:28:30 -0430

    CRUD of Requirement, edit and delete are in develop

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -1,11 +1,6 @@
 import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-<<<<<<< HEAD
-
-=======
-//import { Menu } from './menu/menuJSON';
->>>>>>> 236ec97963fd56954dad84b054f783f717a68c14
 
 
 @Component({
@@ -20,10 +15,6 @@ export class SidebarComponent {
     showMenuReport: string = '';
     showMenuConf: string = '';
     pushRightClass: string = 'push-right';
-<<<<<<< HEAD
-=======
-    //menu: Menu[] = [];
->>>>>>> 236ec97963fd56954dad84b054f783f717a68c14
     @Output() collapsedEvent = new EventEmitter<boolean>();
     
 
--- src/app/layout/recaudo/recaudo.component.ts
+++ src/app/layout/recaudo/recaudo.component.ts
@@ -1,41 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
-import { NgbModal, ModalDismissReasons,NgbDatepickerConfig, NgbDateParserFormatter  } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgbModal, ModalDismissReasons, NgbDatepickerConfig, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { GlobalService } from '../../providers/global.service';
 
 class Recaudo {
-  constructor(
-    public servicio: string = 'Selecciona Servicio'
-  ) {}
+    constructor(
+        public servicio: string = 'Selecciona Servicio'
+    ) { }
 }
 
 
 @Component({
-  selector: 'app-recaudo',
-  templateUrl: './recaudo.component.html',
-  animations: [routerTransition()],
-  styleUrls: ['./recaudo.component.scss']
+    selector: 'app-recaudo',
+    templateUrl: './recaudo.component.html',
+    animations: [routerTransition()],
+    styleUrls: ['./recaudo.component.scss']
 })
 
 
 export class RecaudoComponent implements OnInit {
-closeResult: string;
-  constructor(private modalService: NgbModal) { }
+    closeResult: string;
+    recaudos: any;
+     recaudo: any;
+    selectedRow: number;
+    submitType: string = 'Guardar';
+
+    constructor(private modalService: NgbModal, public globalService: GlobalService) {
+        this.recaudos = [];
+        
+    }
+
+    // recaudo = new this.recaudos();
+    servicios: string[] = ['Venta', 'Compra', 'Arriendo', 'Alquiler'];
 
 
-  servicios: string[] = ['Venta', 'Compra', 'Arriendo', 'Alquiler'];
-
-
-   open(content) {
-
+    open(content) {
+        console.log("aqui");
         this.modalService.open(content).result.then((result) => {
-        this.closeResult = `Closed with: ${result}`;
+            this.closeResult = `Closed with: ${result}`;
+         
 
         }
-        , (reason) => {
-            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        });
+            , (reason) => {
+                this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+            });
     }
 
     private getDismissReason(reason: any): string {
@@ -44,12 +53,44 @@ closeResult: string;
         } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
             return 'by clicking on a backdrop';
         } else {
-            return  `with: ${reason}`;
+            return `with: ${reason}`;
         }
     }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
-   faEdit = faEdit;
+        this.globalService.getModel("/api/requirement")
+            .then((result) => {
+                console.log(result);
+                this.recaudos = result['data'];
+                console.log(this.recaudos);
+            }, (err) => {
+                console.log(err);
+            });
+
+    }
+    faEdit = faEdit;
+
+    onEdit(index: number) {
+        this.selectedRow = index;
+        console.log(index);
+        if(this.submitType==="Guardar"){
+            console.log(this.recaudos);
+
+            this.globalService.updateModel(index, this.recaudos, "/api/requirement")
+            .then((result) => {
+                console.log(result);
+                // this.recaudo = Object.assign({}, this.recaudos[this.selectedRow]);
+            }, (err) => {
+                console.log(err);
+            });
+           
+          }
+       
+
+        
+
+    }
+
 }
+
