f29af9d68aedb16a3dffee99080b648680210ee7
Erick_Perez <erickperezcol@gmail.com>
2018-12-11 23:18:51 -0400

    Eilen haciendo el commit

--- src/app/layout/reports/calificacion/calificacion.module.ts
+++ src/app/layout/reports/calificacion/calificacion.module.ts
@@ -6,11 +6,15 @@ import { CalificacionRoutingModule } from './calificacion-routing.module';
 import { CalificacionComponent } from './calificacion.component';
 import { PageHeaderModule } from '../../../shared';
 import { ChartModule } from 'angular-highcharts';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BsDatepickerModule, DatepickerModule } from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { esLocale } from 'ngx-bootstrap/locale';
+defineLocale('es', esLocale);
 
 @NgModule({
     imports: [
-        BsDatepickerModule.forRoot(), // ToastrModule added
+        BsDatepickerModule.forRoot(), // ToastrModule added        
+        DatepickerModule.forRoot(),
         ChartModule, CommonModule, CalificacionRoutingModule, PageHeaderModule],
     declarations: [CalificacionComponent]
 })
