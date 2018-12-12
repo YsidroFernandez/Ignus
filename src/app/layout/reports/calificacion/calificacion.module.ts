5dc40e469f6dfcdb82e8ea8bc899ac81cdf8a50f
Erick_Perez <erickperezcol@gmail.com>
2018-12-11 20:47:35 -0400

    update add ngx-bootstrap-datepicker in reports

--- src/app/layout/reports/calificacion/calificacion.module.ts
+++ src/app/layout/reports/calificacion/calificacion.module.ts
@@ -6,8 +6,12 @@ import { CalificacionRoutingModule } from './calificacion-routing.module';
 import { CalificacionComponent } from './calificacion.component';
 import { PageHeaderModule } from '../../../shared';
 import { ChartModule } from 'angular-highcharts';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
 @NgModule({
-    imports: [ChartModule, CommonModule, CalificacionRoutingModule, PageHeaderModule],
+    imports: [
+        BsDatepickerModule.forRoot(), // ToastrModule added
+        ChartModule, CommonModule, CalificacionRoutingModule, PageHeaderModule],
     declarations: [CalificacionComponent]
 })
 export class CalificacionModule {}
