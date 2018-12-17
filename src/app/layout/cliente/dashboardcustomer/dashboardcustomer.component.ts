70428831bab7ca35454ad010f7b1db8efb59ce88
Ramon Abreu <abreurm94@gmail.com>
2018-12-17 14:07:04 -0430

    dashboard cliente consumiendo las solicitudes

--- src/app/layout/cliente/dashboardcustomer/dashboardcustomer.component.ts
+++ src/app/layout/cliente/dashboardcustomer/dashboardcustomer.component.ts
@@ -13,6 +13,7 @@ import { GlobalsProvider } from '../../../shared';
 export class DashboardcustomerComponent implements OnInit {
     public numbPage: number;
     public numPage: number;
+    public solicitudSelect: any;
     public pages = 1;
     closeResult: string;
     clientes: any;
@@ -28,40 +29,20 @@ export class DashboardcustomerComponent implements OnInit {
   public listSolicitudes:any;
 
 
-  constructor(private modalService: NgbModal, private globals: GlobalsProvider, public globalService: GlobalService) {
+  constructor(
+      private modalService: NgbModal, 
+      private globals: GlobalsProvider, 
+      public globalService: GlobalService) {
 
     this.clientes = [];
     this.cliente = [];
     this.nuevo = [];
   }
 
-  open(content) {
+ open(content) {
     console.log("aqui");
     this.modalService.open(content).result.then((result) => {
         this.closeResult = `Closed with: ${result}`;
-        if (this.submitType === "Save") {
-           //Aquí no se crean clientes. eso se hace cuando se suscribe
-        }else{
-            this.globalService.updateModel(this.cliente.id, this.cliente, "/api/client")
-                .then((result) => {
-                    if (result['status']) {
-                        //Para que actualice la lista una vez que es editado el cliente
-                        this.globalService.getModel("/api/client")
-                            .then((result) => {
-                                console.log(result);
-                                this.clientes = result['data']['clients'];
-                            }, (err) => {
-                                console.log(err);
-                            });
-                    }
-
-                }, (err) => {
-                    console.log(err);
-                });
-
-        }
-        // Hide cliente entry section.
-        this.showNew = false;
     }, (reason) => {
         this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
@@ -84,30 +65,36 @@ export class DashboardcustomerComponent implements OnInit {
   ngOnInit() {
     this.numPage = this.globals.numPage;       
     this.numbPage = this.globals.numPage;       
-        this.show();
-        this.globalService.getModel("/api/client")
-            .then((result) => {
-                console.log(result);
-                this.clientes = result['data'];
-                console.log(this.clientes);
-            }, (err) => {
-                console.log(err);
-            });
+    this.show();
+    this.globalService.getModel("/api/client")
+    .then((result) => {
+        console.log(result);
+        this.clientes = result['data'];
+        console.log(this.clientes);
+    }, (err) => {
+        console.log(err);
+    });
+
+      this.globalService.getModel('/api/request/pending')
+      .then(res=>{
+        this.listSolicitudes=res['data'];
+        console.log("Las solicitudes: ",this.listSolicitudes);
+      },
+      error=>{
+          console.log(error);
+      })
 
     this.listSeguimiento=[
-      { nro: '12345', descripcion: 'Compra de Casa en el Este de Barqto', estatus:'	Esperando Recaudos'},
-      { nro: '54545', descripcion: 'Alquiler de apartamento', estatus: 'Reserva realizada' }
+        { fecha: "16/11/2018", descripcion: 'Compra de Casa en el Este de Barqto', estatus:'	Esperando Recaudos'},
+        { fecha: "16/12/2018", descripcion: 'Alquiler de apartamento', estatus: 'Reserva realizada' }
 
     ];
-    this.listSolicitudes=[
-      {tipo: "Alquiler", descripcion: "Casa grande con todos los servicios", estado: "En Espera",fecha: "11/10/2018", fotos: []},
-      {tipo: "Compra", descripcion: "Deseo una casa grande", estado: "En Proceso",fecha: "16/11/2018", fotos: []},
-      {tipo: "Venta", descripcion: "Apartamento acogedor con 3 cuartos", estado: "En espera",fecha: "24/09/2018", fotos: []},
-      {tipo: "Arrendamiento", descripcion: "Quinta con 5 cuartos y un baño", estado: "En espera",fecha: "24/09/2018 ", fotos: []},
-      {tipo: "Compra", descripcion: "Apartemento en el centro de la ciudad", estado: "En Revision",fecha: "24/09/2018 ", fotos: []},
-      {tipo: "Compra", descripcion: "casa 2 plantas con garage", estado: "En Revision",fecha: "24/09/2018 ", fotos: []}
-    ]
+
   }
+
+    detallesSolicitud(solicitud){
+        this.solicitudSelect=solicitud;
+    }
   faEye = faEye;
 
 }
