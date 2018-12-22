2497d78abec455311be3d62b8b15b5e72dde76a4
Erick_Perez <erickperezcol@gmail.com>
2018-12-22 00:04:10 -0400

    update

--- src/app/layout/solicitud/solicitud.component.ts
+++ src/app/layout/solicitud/solicitud.component.ts
@@ -1,173 +1,180 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModal, ModalDismissReasons,NgbDatepickerConfig, NgbDateParserFormatter  } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons, NgbDatepickerConfig, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { NgbDateFRParserFormatter } from "./ngb-date-fr-parser-formatter"
 import { faEye, faTimesCircle, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { solicitud, actions, calendariocita, colors } from '../../../environments/environment';
-import { calendarFormat } from 'moment';
 import { GlobalService } from '../../providers/global.service';
-
-import {startOfDay,endOfDay,subDays,addDays,endOfMonth,isSameDay,isSameMonth,addHours} from 'date-fns';
-import {CalendarEvent,CalendarEventAction,CalendarEventTimesChangedEvent,CalendarView,DAYS_OF_WEEK
-} from 'angular-calendar';
+import { GlobalsProvider } from '../../shared';
+import * as moment from 'moment';
+import { routerTransition } from '../../router.animations';
+import * as datepicker from 'ngx-bootstrap/datepicker';
 
 @Component({
     selector: 'app-solicitud',
     templateUrl: './solicitud.component.html',
     styleUrls: ['./solicitud.component.scss'],
-    providers: [{provide: NgbDateParserFormatter, useClass: NgbDateFRParserFormatter}]
+    providers: [ GlobalsProvider],
+    animations: [routerTransition()]
 })
 export class SolicitudComponent implements OnInit {
-  closeResult: string;
-  solicitudes: any;
-  solicitud: any;
-  // It maintains recaudos form display status. By default it will be false.
-  showNew: Boolean = false;
-  // It will be either 'Save' or 'Update' based on operation.
-  submitType: string = 'Save';
-  selectedRow: number;
-  constructor(private modalService: NgbModal, public globalService: GlobalService) {
-    this.solicitudes = [];
-    this.solicitud = [];
-  }
-  
 
-  solicitudapprove = {
-    EmployeeId: "",
-    title: "",
-    start: new Date(),
-    end: new Date(),
-    color: colors.red,
-    resizable: {
-      beforeStart: true,
-      afterEnd: true
-    },
-    draggable: true
-   }
+    datePickerConfig: Partial<datepicker.BsDatepickerConfig>;
+    public numPage: number;
+    public pages = 1;
+    closeResult: string;
+    solicitudes: any;
+    solicitud: any;
+    // It maintains recaudos form display status. By default it will be false.
+    showNew: Boolean = false;
+    // It will be either 'Save' or 'Update' based on operation.
+    submitType: string = 'Save';
+    selectedRow: number;
+    constructor(
+        private globals: GlobalsProvider,
+        private modalService: NgbModal,
+        public globalService: GlobalService) {
+        this.solicitudes = [];
+        this.solicitud = [];
+        this.datePickerConfig = Object.assign({},
+            { containerClass: 'theme-dark-blue' },
+            { showWeekNumbers: false },
+            { dateInputFormat: 'DD/MM/YYYY' },
+            { locale: 'es' });
+    }
 
-  show() {
-    console.log("aqui va el loader");
-}
+    ngOnInit() {
+        this.numPage = this.globals.numPage;    
+        this.allSolicitud();
+     
+    }
 
-ngOnInit() {
-    this.show();
-    this.globalService.getModel("/api/request/pending")
-        .then((result) => {
-            console.log(result);
-            this.solicitudes = result['data'];
-            
-        }, (err) => {
-            console.log(err);
-        });
-}
-// This method associate to Edit Button.
-onEdit(index: number) {
- // Assign selected table row index.
- this.selectedRow = index;
- // Initiate new solicitud.
- 
- // Retrieve selected solicitud from list and assign to model.
- this.solicitud = Object.assign({}, this.solicitudes[this.selectedRow]);
- console.log(this.solicitud)
- // Change submitType to Update.
- this.submitType = 'Actualizar';
- // Display solicitud entry section.
- this.showNew = true;
-}
 
-    // This method associate to Delete Button.
+    solicitudapprove = {
+        EmployeeId: "",
+        title: "",
+        start: new Date(),
+        end: new Date(),
+        color: colors.red,
+        resizable: {
+            beforeStart: true,
+            afterEnd: true
+        },
+        draggable: true
+    }
+
+   
+   
+    onEdit(index: number) {
+        this.selectedRow = index;
+       this.solicitud = Object.assign({}, this.solicitudes[this.selectedRow]);
+        console.log(this.solicitud)
+        this.submitType = 'Actualizar';
+       this.showNew = true;
+    }
+
     onDelete(index: number) {
-      console.log('eliminando');
-      this.selectedRow = index;
-      this.solicitud = Object.assign({}, this.solicitudes[this.selectedRow]);
-      this.showNew = true;
-      //Pendiente
-      if(confirm('¿Estas seguro de eliminar este usuario?')){
-          this.globalService.updateModel(this.solicitud.id,{message:"no a guta"},"/api/request/reject")
-                  .then((result) => {
-                      console.log(result);
-                      if (result['status']) {
+        console.log('eliminando');
+        this.selectedRow = index;
+        this.solicitud = Object.assign({}, this.solicitudes[this.selectedRow]);
+        this.showNew = true;
+        //Pendiente
+        if (confirm('¿Estas seguro de eliminar este usuario?')) {
+            this.globalService.updateModel(this.solicitud.id, { message: "no a guta" }, "/api/request/reject")
+                .then((result) => {
+                    console.log(result);
+                    if (result['status']) {
                         //this.solicitudes.splice(this.selectedRow, 1);                     
-                      }
+                    }
 
-                  }, (err) => {
-                      console.log(err);
-                  });
-          }
-          this.solicitudes.splice(this.selectedRow, 1);
-  }
+                }, (err) => {
+                    console.log(err);
+                });
+        }
+        this.solicitudes.splice(this.selectedRow, 1);
+    }
 
-      // This method associate toCancel Button.
-      onCancel() {
+    // This method associate toCancel Button.
+    onCancel() {
         // Hide Usuario entry section.
         this.showNew = false;
     }
 
 
- open(content) {
-     this.modalService.open(content).result.then((result) => {
+    open(content) {
+        this.modalService.open(content).result.then((result) => {
 
-     this.closeResult = `Closed with: ${result}`;
-      if (this.submitType === 'save') {
-   // Push solicitud model object into solicitud list.
-   this.solicitudes.push(this.solicitud);
-   console.log(this.submitType);
- } else {
-   this.submitType = 'update';
-   // Update the existing properties values based on model
-   this.solicitudes[this.selectedRow].email = this.solicitud.email;
-   this.solicitudes[this.selectedRow].type = this.solicitud.type;
-   this.solicitudes[this.selectedRow].description = this.solicitud.description;
-   this.solicitudes[this.selectedRow].state = this.solicitud.estate;
-   this.solicitudes[this.selectedRow].date = this.solicitud.date;
+            this.closeResult = `Closed with: ${result}`;
+            if (this.submitType === 'save') {
+                // Push solicitud model object into solicitud list.
+                this.solicitudes.push(this.solicitud);
+                console.log(this.submitType);
+            } else {
+                this.submitType = 'update';
+                // Update the existing properties values based on model
+                this.solicitudes[this.selectedRow].email = this.solicitud.email;
+                this.solicitudes[this.selectedRow].type = this.solicitud.type;
+                this.solicitudes[this.selectedRow].description = this.solicitud.description;
+                this.solicitudes[this.selectedRow].state = this.solicitud.estate;
+                this.solicitudes[this.selectedRow].date = this.solicitud.date;
 
-   this.solicitud = Object.assign({}, this.solicitudes[this.selectedRow]);
+                this.solicitud = Object.assign({}, this.solicitudes[this.selectedRow]);
 
-   console.log( this.solicitudes[this.selectedRow]);
-   console.log(this.submitType);
- }
- // Hide solicitud entry section.
- this.showNew = false;
-     }
-     , (reason) => {
-         this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-     });
- }
-
- private getDismissReason(reason: any): string {
-     if (reason === ModalDismissReasons.ESC) {
-         return 'by pressing ESC';
-     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-         return 'by clicking on a backdrop';
-     } else {
-         return  `with: ${reason}`;
-     }
- }
-
-
- aceptar(i){
-    console.log(this.solicitudes[i-1])
-    this.globalService.updateModel(this.solicitudes[i-1].id,{EmployeeId:1,date:"12/12/2018"}, "/api/request/pending/approve")
-    .then((result) => {
-        console.log(result);
-        if (result['status']) {
-            //Para que actualice la lista una vez que es eliminado la solicitud
-                    console.log(result);
+                console.log(this.solicitudes[this.selectedRow]);
+                console.log(this.submitType);
+            }
+            // Hide solicitud entry section.
+            this.showNew = false;
         }
+            , (reason) => {
+                this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+            });
+    }
 
-    }, (err) => {
-        console.log(err);
-    });
-   this.solicitudes.splice(i, 1);
-   alert("aceptado y validada la cita")
- }
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
+    }
 
 
- faEye = faEye;
- faEdit = faEdit;
- faTrash = faTrash;
- faCheck =  faCheckCircle;
- faCancel = faTimesCircle;
- msg = '';
+    aceptar(i) {
+        console.log(this.solicitudes[i - 1])
+        this.globalService.updateModel(this.solicitudes[i - 1].id, { EmployeeId: 1, date: "12/12/2018" }, "/api/request/pending/approve")
+            .then((result) => {
+                console.log(result);
+                if (result['status']) {
+                    //Para que actualice la lista una vez que es eliminado la solicitud
+                    console.log(result);
+                }
+
+            }, (err) => {
+                console.log(err);
+            });
+        this.solicitudes.splice(i, 1);
+        alert("aceptado y validada la cita")
+    }
+
+
+    faEye = faEye;
+    faEdit = faEdit;
+    faTrash = faTrash;
+    faCheck = faCheckCircle;
+    faCancel = faTimesCircle;
+    msg = '';
+
+   
+    allSolicitud() {        
+        this.globalService.getModel("/api/request/pending").then((result) => {
+            console.log(result);
+            this.solicitudes = result['data'];
+        }, (err) => {
+            console.log(err);
+        });
+    }
 }
--- src/app/layout/solicitud/solicitud.module.ts
+++ src/app/layout/solicitud/solicitud.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgbModule,NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { NgxPaginationModule } from 'ngx-pagination';
 import { SolicitudRoutingModule } from './solicitud-routing.module';
 import { SolicitudComponent } from './solicitud.component';
 import { PageHeaderModule } from '../../shared';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BsDatepickerModule, DatepickerModule } from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { esLocale } from 'ngx-bootstrap/locale';
+defineLocale('es', esLocale);
 
 @NgModule({
-    imports: [CommonModule, SolicitudRoutingModule,
-       PageHeaderModule,
-       FontAwesomeModule, NgbModule.forRoot(),
-        FormsModule, ReactiveFormsModule],
-    declarations: [SolicitudComponent],
-    providers: [
+  imports: [
+    NgxPaginationModule,
+    CommonModule, SolicitudRoutingModule,
+    PageHeaderModule,
+    BsDatepickerModule.forRoot(), // ToastrModule added        
+    DatepickerModule.forRoot(),
+    FontAwesomeModule, NgbModule.forRoot(),
+    FormsModule, ReactiveFormsModule],
+  declarations: [SolicitudComponent],
+  providers: [
     NgbActiveModal,
   ]
 
