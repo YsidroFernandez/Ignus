e7025e93fac888844fa68f04135d213692389bd3
jonathanfalcon95 <jonathan.falcon.95@hotmail.com>
2019-01-15 19:43:30 -0400

    changes to dashboard

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
-
-
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { single } from './data';
 
 @Component({
     selector: 'app-dashboard',
@@ -10,10 +10,29 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class DashboardComponent implements OnInit {
+    
+    
+    single: any[];
+    multi: any[];
+  
+    view: any[] = [700, 400];
+  
+    // options
+    showYAxis = true;
+    gradient = false;
+    showLegend = true;
+    showXAxisLabel = true;
+    xAxisLabel = 'Country';
+    showYAxisLabel = true;
+    yAxisLabel = 'Population';
+  
+    colorScheme = {
+      domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    };
   
 
     constructor() {
-      
+        Object.assign(this, { single })
     }
 
     ngOnInit() {}
