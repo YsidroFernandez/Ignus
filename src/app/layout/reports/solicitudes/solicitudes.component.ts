1e5e163e18a9f21f4a6df495fd4485ee63c9aa41
PedroD <peds.gado@gmail.com>
2019-01-16 04:48:25 -0400

    tengo sueño

--- src/app/layout/reports/solicitudes/solicitudes.component.ts
+++ src/app/layout/reports/solicitudes/solicitudes.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ElementRef ,ViewChild } from '@angular/core';
 import { routerTransition } from '../../../router.animations';
 import { NgbModal, ModalDismissReasons, NgbDatepickerConfig, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { Chart } from 'angular-highcharts';
+import { GlobalService } from '../../../providers/global.service';
+import { NgxCoolDialogsService } from 'ngx-cool-dialogs';
 import * as moment from 'moment';
 import * as datepicker from 'ngx-bootstrap/datepicker';
 import * as jspdf from 'jspdf';  
@@ -19,17 +21,39 @@ export class SolicitudesComponent implements OnInit {
     values = ['circular', 'barra', 'lineal'];
     defaultValue = this.values[0];
     tipos: any = [{ id: 1, name: "circular" }, { id: 2, name: "barra" }, { id: 3, name: "lineal" }];
-
+    imagen: any;
+    agencia: any;
+    agencias: any;
     public view = false;
     public chart: any;
-    constructor() {
+    constructor(private modalService: NgbModal, public globalService: GlobalService, private coolDialogs: NgxCoolDialogsService) {
         let now = moment().format();
         console.log('hello world', this.tipos);
 
         var doc = new jspdf('p', 'pt');
     }
+
+    convertImgToBase64URL(url, callback){
+        var img = new Image();
+        img.crossOrigin = 'Anonymous';
+        img.onload = function(){
+            var canvas = document.createElement('canvas');
+            var ctx = canvas.getContext('2d'), dataURL;
+            canvas.height = img.height;
+            canvas.width = img.width;
+            ctx.drawImage(img, 0, 0);
+            dataURL = canvas.toDataURL("image/png");
+            callback(dataURL);
+            canvas = null; 
+        };
+        img.src = url;
+    }
+
+
     downloadImagePDF(){
-        var doc = new jspdf()
+        this.convertImgToBase64URL('https://ignus-backend-jchiquin.c9users.io/public/imgs/logo/basic-logo.png', (base64Img) =>{
+            this.imagen = base64Img; // myBase64 is the base64 string
+            var doc = new jspdf()
         var data = document.getElementById('content');  
         html2canvas(data).then(canvas => {  
           // Few necessary setting options  
@@ -39,22 +63,42 @@ export class SolicitudesComponent implements OnInit {
           var heightLeft = imgHeight;  
       
           const contentDataURL = canvas.toDataURL('image/png')  
-          //let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
           var position = 0;  
-          //pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
-          doc.addImage(contentDataURL, 'PNG', 0, 40, imgWidth, imgHeight) 
+          doc.addImage(contentDataURL, 'PNG', 0, 75, imgWidth, imgHeight)
+          doc.setFontSize(10)
+          doc.text(65, 25, this.agencias.name+" "+this.agencias.rif)
+          doc.setFontSize(10) 
+          let middleUbication = this.agencias.ubication.lastIndexOf(' ',this.agencias.ubication.length/2)
+          doc.text(50, 30, this.agencias.ubication.substr(0,middleUbication))
+          doc.text(50, 35, this.agencias.ubication.substr(middleUbication+1,this.agencias.ubication.length))
+          doc.setFontSize(10)
+          doc.text(55, 40, this.agencias.phoneNumber+ " / " +this.agencias.phoneNumber2) 
           doc.setFontSize(30)
-          doc.text(55, 25, 'Reportes Estadisticos')
-          var img = new Image();
-          img.src = "./../../assets/images/ignus3.png"
-          doc.addImage(img, 'PNG', 0,3,30,30)
-          doc.addImage(img, 'PNG', 180,3,30,30)
+          doc.text(55, 60, 'Reportes Estadisticos')
+          console.log(this.imagen);
+          doc.addImage(this.imagen, 'PNG', 10,3,30,30)
+          doc.addImage(this.imagen, 'PNG', 170,3,30,30)
+
           doc.save("Reporte.pdf")
-        });  
-    
+        }); 
+        });
+         
+        
           }
 
+    allAgency(){
+        this.globalService.getModel("/api/agency")
+        .then((result) => {
+            console.log(result);
+            this.agencias = result['data'];
+            console.log(this.agencias);
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
     ngOnInit() {
+        this.allAgency();
 
     }
 
