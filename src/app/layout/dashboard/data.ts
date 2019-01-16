5168fe281b8af4ba6f51d48692d7f205b479eb3c
jonathanfalcon95 <jonathan.falcon.95@hotmail.com>
2019-01-16 07:11:16 -0400

    dashboard v1.0

--- src/app/layout/dashboard/data.ts
+++ src/app/layout/dashboard/data.ts
@@ -1,15 +1,37 @@
 export var single = [
     {
-      "name": "Germany",
-      "value": 8940000
+      "name": "Alquileres",
+      "value": 894
     },
     {
-      "name": "USA",
-      "value": 5000000
+      "name": "Ventas",
+      "value": 500
     },
     {
-      "name": "France",
-      "value": 7200000
+      "name": "Compras",
+      "value": 720
+    },
+    {
+      "name": "Arrendamientos",
+      "value": 720
+    }
+  ];
+  export var single2 = [
+    {
+      "name": "Casas",
+      "value": 894
+    },
+    {
+      "name": "Apartamentos",
+      "value": 500
+    },
+    {
+      "name": "Terrenos",
+      "value": 720
+    },
+    {
+      "name": "Local Comercial",
+      "value": 720
     }
   ];
   
@@ -90,3 +112,65 @@ export var single = [
         }
         ]
     },]
+
+   export var lineData= [
+      {
+        "name": "Germany",
+        "series": [
+          {
+            "name": "2010",
+            "value": 7300000
+          },
+          {
+            "name": "2011",
+            "value": 8940000
+          },
+          {
+            "name": "2012",
+            "value": 730000
+          },
+          {
+            "name": "2013",
+            "value": 8940000
+          },
+          {
+            "name": "2014",
+            "value": 5300000
+          },
+          {
+            "name": "2015",
+            "value": 4940000
+          }
+        ]
+      },
+    ​
+      {
+        "name": "USA",
+        "series": [
+          {
+            "name": "2010",
+            "value": 7870000
+          },
+          {
+            "name": "2011",
+            "value": 8270000
+          },
+          {
+            "name": "2012",
+            "value": 7000000
+          },
+          {
+            "name": "2013",
+            "value": 6940000
+          },
+          {
+            "name": "2014",
+            "value": 3300000
+          },
+          {
+            "name": "2015",
+            "value": 5540000
+          }
+        ]
+      }
+    ]
