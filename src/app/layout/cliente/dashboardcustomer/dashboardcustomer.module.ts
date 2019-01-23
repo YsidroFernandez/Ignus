71122a3f2b144bf4a8d1b70add053c1c1ac4111a
wquerales <thetigre123@gmail.com>
2019-01-22 23:33:34 -0400

    arreglos del dashoard cliente

--- src/app/layout/cliente/dashboardcustomer/dashboardcustomer.module.ts
+++ src/app/layout/cliente/dashboardcustomer/dashboardcustomer.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule,registerLocaleData } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashboardcustomerRoutingModule } from './dashboardcustomer-routing.module';
 import { DashboardcustomerComponent } from './dashboardcustomer.component';
@@ -7,6 +7,18 @@ import { PageHeaderModule } from '../../../shared';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DashboardCustomerFilterPipe } from './dashboardcustomer-filter.pipe';
+import { StatModule } from '../../../shared';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FlatpickrModule } from "angularx-flatpickr";
+import { BsDatepickerModule, DatepickerModule } from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { esLocale } from 'ngx-bootstrap/locale';
+defineLocale('es', esLocale);
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs);
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 @NgModule({
   imports: [
     CommonModule,
@@ -14,7 +26,15 @@ import { DashboardCustomerFilterPipe } from './dashboardcustomer-filter.pipe';
     DashboardcustomerRoutingModule,
     PageHeaderModule,
     FontAwesomeModule,
-    FormsModule, ReactiveFormsModule
+    FormsModule, ReactiveFormsModule,
+    StatModule,
+    BsDatepickerModule.forRoot(), // ToastrModule added        
+    DatepickerModule.forRoot(),
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
   declarations: [DashboardcustomerComponent,DashboardCustomerFilterPipe],
   providers: []
